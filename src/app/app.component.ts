f294a5b8ab0640d47d9fce77af30e82f34fa3d94
Amal Mathew Biju <amalmathewbiju@gmail.com>
2024-11-26 09:09:53 +0530

    fourth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,7 @@ import { ContactComponent } from './contact/contact.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { DataService } from './data.service';
-import { TaskService } from './task.service';
+
 import { FooterComponent } from './footer/footer.component';
 import { ChatAssistantComponent } from './chat-assistant/chat-assistant.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -33,18 +32,9 @@ import { LoadingComponent } from './loading/loading.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  tasks: any[] = [];
+export class AppComponent  {
 
-  constructor(private taskService: TaskService) {
-    // alert("Open in Laptop for better experience")
-  }
-
-  ngOnInit() {
-    this.taskService.getTasks().subscribe((data) => {
-      this.tasks = data;
-    });
-  }
+  
 
   // Disable right-click
   @HostListener('document:contextmenu', ['$event'])
